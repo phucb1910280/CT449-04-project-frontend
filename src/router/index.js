2f939f912ea3c7957b9e53cf6a90a79267cbb425
Dao Vinh Phuc <daovinhphuc.1632@gmail.com>
2023-04-22 20:48:20 +0700

    Add route

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CartPage from '../views/CartPage.vue'
+import ProductDetailPage from '../views/ProductDetailPage.vue'
+import ProductsPage from '../views/ProductsPage.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/products',
+    name: 'Products',
+    component: ProductsPage,
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/products/:id',
+    name: 'ProductDetail',
+    component: ProductDetailPage,
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: CartPage,
+  },
+  {
+    path: '/',
+    redirect: '/products',
   }
 ]
 
---
+++ src/views/CartPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>This is Shopping Cart Page</h1>
+</template>
+
+<script scoped>
+    export default {
+        name: 'CartPage',
+    };
+</script>
+
+<style>
+    h1 {
+        color: green;
+    }
+</style>
